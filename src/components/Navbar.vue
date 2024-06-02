3374e56f01abb5b2393855189cf0040e9e9457fc
Agassi Eribo <eriboonshil2004@gmail.com>
2024-06-02 17:51:47 +0100

    Initial commits

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<script setup>
+  import gitHubIcon from './icons/gitHubIcon.vue';
+</script>
+
+<template>
+
+  <nav>
+    <div class="navbar">
+      <gitHubIcon class="github-icon"/>
+      <h1>AgassiEribo's GitHub Portfolio.</h1>
+    </div>
+  </nav>
+
+</template>
+
+<style scoped>
+
+.navbar {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  justify-content: center;
+  background-color: #000000;
+  color: #e3d1e3;
+  font-style: oblique;
+  text-align: center;
+  padding: 20px;
+  margin-bottom: 100px
+}
+
+.github-icon {
+  font-size: 3rem;
+}
+</style>
